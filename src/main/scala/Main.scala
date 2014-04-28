014edfaae7a6d4809b696248246b7f6ca7610e6d
Greg Nwosu <greg.nwosu@gmail.com>
2014-04-28 09:18:30 +0100

    exercise 3/4

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,24 @@
 
 object Hello{
-
-  def main(args: Array[String]) :Unit  = {
+ 
+ def main(args: Array[String]) :Unit  = {
     println("Hello World")
     println("this is a test")
   }
 
- def partial[A,B,C](a:A , f: (A,B)=>C): B => C =
- (b:B) => f (a, b) 
+ def partial[A,B,C](a:A , f:  (A,B) => C ):  (B => C) =
+ (b:B) => f (a, b)
+
+
+
+def curry[A,B,C](f:(A,B)=>C):A => (B => C) =
+ (a:A) =>  (b:B) => f(a,b)
+
+
+
+
+def uncurry[A,B,C](f: A => B=> C): (A,B)=>C =
+(a:A,b:B) => f(a)(b)
+
 
 }
